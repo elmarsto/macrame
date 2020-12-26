93fb00ecffe9db94f9d0f2362ab239e17bec7b73
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-25 18:04:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Macrame
+
+A set of rust macros that extend wasm_bindgen
+Associated with //github.com/elmarsto/spar-rs, which
+generates code that in turn uses macrame as a dependency.
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro]
+h
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
